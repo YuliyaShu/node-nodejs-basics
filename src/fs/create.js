b9fd5636d0bcff1888f5cd72c56c49383a948fbb
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-01 15:38:26 +0100

    feat: fs create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const pathToFolder = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files');
+const pathToFresh = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files/fresh.txt');
+const textToFresh = 'I am fresh and young';
 const create = async () => {
-    // Write your code here 
+    fs.readdir(pathToFolder, (err, files) => {
+        if (err) throw err;
+        if (files.includes('fresh.txt')) {
+            throw new Error('FS operation failed');
+        } else {
+            fs.appendFile(pathToFresh, textToFresh, (err) => {
+                if (err) throw err;
+            } )
+        };  
+    });
 };
 
 await create();
