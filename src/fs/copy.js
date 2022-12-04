1a3cb4730513d090916db1e9f96ed6e27edad668
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-04 13:23:00 +0100

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const pathToFolder = path.join(dirName, '/files');
+const pathToNewFolder = path.join(dirName, '/files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(pathToFolder); 
+        await mkdir(pathToNewFolder);
+        for (const file of files) {
+            await copyFile(path.join(pathToFolder, file), path.join(pathToNewFolder, file));
+        }
+    } catch (err) {
+        if (err) throw new Error('FS operation failed');
+    }
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -2,17 +2,18 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const fileName = path.dirname(fileURLToPath(import.meta.url));
-const pathToFolder = path.join(fileName, '/files');
-const pathToFresh = path.join(fileName, '/files/fresh.txt');
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const pathToFolder = path.join(dirName, '/files');
+const pathToFile = path.join(pathToFolder, '/fresh.txt');
 const textToFresh = 'I am fresh and young';
+
 const create = async () => {
     fs.readdir(pathToFolder, (err, files) => {
         if (err) throw err;
         if (files.includes('fresh.txt')) {
             throw new Error('FS operation failed');
         } else {
-            fs.writeFile(pathToFresh, textToFresh, (err) => {
+            fs.writeFile(pathToFile, textToFresh, (err) => {
                 if (err) throw err;
             } )
         };  
