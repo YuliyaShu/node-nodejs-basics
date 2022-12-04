bcd34cbec2cc952110b8248533aa3e49c285d989
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-04 18:57:15 +0100

    feat: streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const fileName = 'fileToWrite.txt';
+const pathToFolder = path.join(dirName, '/files');
+const pathToFile = path.join(pathToFolder, fileName);
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const stream = createWriteStream(pathToFile);
+        process.stdout.write("Hello! Insert text. (To exit, press ctr+c)");
+        process.on('SIGINT', () => {
+            process.stdout.write('Buy!');
+            process.exit();
+        });
+        process.stdin.on('data', data => stream.write(data.toString()));
+    } catch (err) {
+        if (err) throw new Error(err);
+    }
 };
 
 await write();
