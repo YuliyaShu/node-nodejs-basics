bda102e97bd0cd72f9d5ce97054b62a81e0e7e4c
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-04 18:23:44 +0100

    feat: streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const fileName = 'fileToRead.txt';
+const pathToFolder = path.join(dirName, '/files');
+const pathToFile = path.join(pathToFolder, fileName);
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const stream = createReadStream(pathToFile, 'utf-8');
+        stream.on('data', (data) => process.stdout.write(data));
+    } catch (err) {
+        if (err) throw new Error(err);
+    }
 };
 
 await read();
