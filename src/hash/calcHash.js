27b31600e4e91f0216a2e224004eeafaaf2a0f1d
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-04 17:37:41 +0100

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'node:crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'node:fs/promises';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const pathToFolder = path.join(dirName, '/files');
+const fileName = 'fileToCalculateHashFor.txt';
+const pathToFile = path.join(pathToFolder, fileName);
+const fileContent = await readFile(pathToFile);
+const hash = createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+    hash.update(fileContent);
+    console.log(hash.digest('hex')); 
 };
 
 await calculateHash();
