5cc9c19f02a09b297be703d4507beb2b28be1eac
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-04 15:45:10 +0100

    feat: cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arr = process.argv
+        .slice(2)
+        .map((item) => (item.startsWith('--')) 
+            ? item.slice(2) 
+            : item);
+    for (let i = 0; i < arr.length; i += 2) {
+        console.log(`${arr[i]} is ${arr[i + 1]}`)
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,7 @@
 const parseEnv = () => {
-    process.env.RSS_foo='bar';
-    process.env.RSS_bar='baz';
     const obj = process.env;
+    obj.RSS_foo='bar';
+    obj.RSS_bar='baz';
     Object.entries(obj).forEach(([key, value]) => {
         if (key.startsWith('RSS_')) {
             console.log(`${key}=${value}`);
