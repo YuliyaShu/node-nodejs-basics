c6b34b68190e35661694dac5d7bf0800be9cad5b
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-05 09:59:22 +0100

    feat: wt/worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { Worker } from 'node:worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const pathToWorker = path.join(dirName, './worker.js');
+const data = 11;
+
+const worker = new Worker(pathToWorker, {
+    workerData: data,
+});
+
 const performCalculations = async () => {
-    // Write your code here
+    worker.on('message', msg => console.log(`Fibonacci result: ${msg}`));
+    worker.on('error', (err) => {
+        throw new Error(err);
+    });
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
 };
 
 await performCalculations();
