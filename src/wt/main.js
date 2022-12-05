5550427669c7fd2f9e6056d8c4bf01a7dbdcb2ee
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-05 15:51:20 +0100

    feat: wt/main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,6 +1,7 @@
 import { Worker } from 'node:worker_threads';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { writeFile } from 'node:fs/promises';
 
 const dirName = path.dirname(fileURLToPath(import.meta.url));
 const pathToWorker = path.join(dirName, './worker.js');
@@ -8,7 +9,7 @@ const data = 11;
 const worker = new Worker(pathToWorker, {
     workerData: data,
 });
-worker.on('message', msg => console.log(`Fibonacci result: ${msg}`));
+worker.on('message', msg => console.log(`Fibonacci result: ${msg[0]}`));
     worker.on('error', (err) => {
         throw new Error(err);
     });
@@ -18,7 +19,24 @@ worker.on('message', msg => console.log(`Fibonacci result: ${msg}`));
       });
 
 const performCalculations = async () => {
+  worker.on('message', async msg => {
+    const result = [];
     
+    for (let i = 0; i < msg[1]; i += 1) {
+      const pathToIWorker = path.join(dirName, `./worker${i}.js`);
+      const workerCPU = new Worker(pathToIWorker);
+      await writeFile(pathToIWorker, "import { parentPort } from 'node:worker_threads'; \n parentPort.on('message', msg => parentPort.postMessage(msg));");
+      workerCPU.postMessage(10 + i);
+      result.push(new Promise((res, rej) => {
+        workerCPU.on('message', msg => res({ status: 'resolved', data: msg }));
+        workerCPU.on('error', () => rej({ status: 'error', data: null }));
+      }));
+    }
+    Promise.all(result).then((value) => {
+      console.log('🚀 ~ Promise.all ~ value', value);
+      process.exit();
+    })
+  })
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,8 @@
 import { parentPort, workerData } from 'node:worker_threads';
+import os from 'node:os';
 
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-const sendResult = () => parentPort.postMessage(nthFibonacci(workerData));
+const countOfWorkers = os.cpus().length;
+const sendResult = () => parentPort.postMessage([nthFibonacci(workerData), countOfWorkers]);
 
 sendResult();
